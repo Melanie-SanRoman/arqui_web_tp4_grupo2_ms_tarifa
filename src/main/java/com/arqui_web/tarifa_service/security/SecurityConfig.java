410c8c9dde4cc020898f19123248e5eabf6bab97
Melanie San Roman <srmel123@gmail.com>
2025-11-26 17:19:13 -0300

    Correccion de sintaxis

--- src/main/java/com/arqui_web/tarifa_service/security/SecurityConfig.java
+++ src/main/java/com/arqui_web/tarifa_service/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.arqui_web.tarifa_service.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,23 +16,15 @@ public class SecurityConfig {
 	@Autowired
 	private JwtAuthFilter jwtAuthFilter;
 
-	private static final String[] SWAGGER_WHITELIST = 
-		{ 
-			"/swagger-ui/**", 
-			"/swagger-ui.html", 
-			"/v3/api-docs/**",
-			"/v3/api-docs", 
-			"/api-docs", 
-			"/api-docs/**" 
-		};
+	private static final String[] SWAGGER_WHITELIST = { "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**",
+			"/v3/api-docs", "/api-docs", "/api-docs/**" };
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http.csrf(csrf -> csrf.disable()).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-				.authorizeHttpRequests(auth -> auth
-						.requestMatchers(SWAGGER_WHITELIST).permitAll() // Swagger libre
-						.anyRequest().authenticated());
+				.authorizeHttpRequests(auth -> auth.requestMatchers(SWAGGER_WHITELIST).permitAll()
+						.requestMatchers(HttpMethod.PUT, "/tarifas/").hasRole("ADMIN").anyRequest().authenticated());
 
 		return http.build();
 	}
